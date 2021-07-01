7ba7b0f4320e96c136c3f44c056816e781289e8d
HeejinPark-kor <qkrgmlwls104@gmail.com>
2021-07-02 02:10:17 +0900

    programmers12927

---
+++ 210702_programmers12927/solution.py
@@ -0,0 +1,12 @@
+import heapq
+
+
+def solution(n, works):
+    if n >= sum(works):
+        return 0
+    works = [-i for i in works]
+    heapq.heapify(works)
+    for _ in range(n):
+        w = heapq.heappop(works) + 1
+        heapq.heappush(works, w)
+    return sum([i**2 for i in works])
